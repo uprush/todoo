fdcd6fd33128c16756c697e20f5febf222bc4f20
Yifeng Jiang <uprushworld@gmail.com>
2014-08-13 14:39:42 +0900

    Initial Commit

---
+++ ToDoo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoo
+//
+//  Created by Jiang, Yifeng on 8/13/14.
+//  Copyright (c) 2014 Uprush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
